19f42c934d47b21550e3b158debc30cd4a49ce4b
Maryna <m.ohorodnik.22@gmail.com>
2020-09-22 10:04:52 +0300

    Add project files.

---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("c = ");
+            long c = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+        }
+    }
+}
